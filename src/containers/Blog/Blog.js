05595f9413620bd07cca2bb50b1543526d2e397b
dstark88 <starkwalker88@gmail.com>
2019-07-17 14:49:55 -0600

    starting posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,23 +7,28 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
 
     componentDidMount () {
         axios
-            .get('https://jsonplaceholder.typicode.com/post')
+            .get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                console.log(response);
+                this.setState({ posts: response.data });
+                // console.log("res: ", response);
             });
             
     }
 
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post title={post.title}/>
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
